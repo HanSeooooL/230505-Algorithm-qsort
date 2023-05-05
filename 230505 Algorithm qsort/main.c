763062d221785a3e1a4c77c5d5b024bfb9824514
설 한 <5fz6hc6psp@privaterelay.appleid.com>
2023-05-05 19:13:30 +0900

    Initial Commit

---
+++ 230505 Algorithm qsort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  230505 Algorithm qsort
+//
+//  Created by 한설 on 2023/05/05.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
